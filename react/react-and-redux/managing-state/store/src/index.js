4a475e4c4a563bfb7fd816ca00f0509f5d87bb68
viviruska <vivien.ruska@prezi.com>
2021-02-10 00:39:09 +0100

    feat: Add store management lib with ADD_TODO, REMOVE_TODO, TOGGLE_TODO actions

---
+++ react/react-and-redux/managing-state/store/src/index.js
@@ -0,0 +1,64 @@
+// Library Code
+function createStore (reducer) {
+  // The store should have 4 parts
+  // 1. The state
+  // 2. Get the state
+  // 3. Listen to changes on the state
+  // 4. Update the state
+
+  let state
+  let listeners = []
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.array.forEach(listener => listener());
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
+}
+
+
+// App Code
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  } else if (action.type === 'REMOVE_TODO') {
+    return state.filter((todo) => todo.id !== action.id)
+  } else if (action.type === 'TOGGLE_TODO') {
+    return state.map((todo) => todo.id !== action.id ? todo : 
+    Object.assign({}, todo, { complete: !todo.complete }))
+  } else {
+    return state
+  }
+  
+}
+
+const store = createStore(todos)
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+// const unsubscribe = store.subscribe(() => {
+//   console.log('The store changed.')
+// })
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+})
+// unsubscribe()
